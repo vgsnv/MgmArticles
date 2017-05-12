ef175e543bfb3c3b82fada74cbca147ec8645f6f
vgsnv <vgsnv@yandex.ru>
2017-05-12 23:40:43 +0300

    remove articleList children

--- src/components/ArticleList/ArticleList.tsx
+++ src/components/ArticleList/ArticleList.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as css from './ArticleList.styl';
 
 import { Article, Props as ArticleProps, Dispatch as ArticleDispatch } from './Article/Article';
-import { ArticleListHeader } from './ArticleListHeader/ArticleListHeader';
 import { State } from 'flow/model';
  
 export interface Props{
@@ -21,6 +20,26 @@ interface State{
 };
 
 export class ArticleList extends React.Component<Props & Dispatch, State> {
+
+  private getArticleListHeader(){
+
+      console.log('ArticleListHeader props', this.props);
+
+      const {
+        articlesMode,
+        onClickTitle,
+        onClickValue
+      } = this.props
+
+      return(<thead id={css.articleListHeader}>
+        <tr>
+          { articlesMode === 'DELETING' &&
+          <th>Выбрать</th> }
+          <th onClick={onClickTitle} >Заголовок</th>
+          <th onClick={onClickValue} >Значение</th></tr>
+      </thead>);
+
+  }
 
   private getArticles(){
 
@@ -42,9 +61,21 @@ export class ArticleList extends React.Component<Props & Dispatch, State> {
         value: article.value
       }
 
-      return(<Article
-              key={article.id}
-              {...ItemArticle}/>);
+       const onInputClick = (e) =>{
+
+        e.preventDefault();
+
+        onSelectClick(article.id);
+
+      }
+
+      return(<tr key={article.id} className={css.article}>
+        { articlesMode === 'CHANGING' &&
+        <td className={css.articleSelect}><input onClick={onInputClick} type="checkbox" /> </td> }
+        <td className={css.artilceTitle} >{article.title}</td>
+        <td className={css.artilceValue}>{article.value}</td>
+      </tr>);
+
     })
     
   };
@@ -54,19 +85,11 @@ export class ArticleList extends React.Component<Props & Dispatch, State> {
     console.log('ArticleList props', this.props);
 
     const getArticles = this.getArticles();
-
-    const {
-      articlesMode,
-      onClickTitle,
-      onClickValue
-    } = this.props
-
+    const getArticleListHeader = this.getArticleListHeader();
+    
     return(<table id={css.articleList}>
-      <ArticleListHeader
-        articlesMode = {articlesMode}
-        onClickHeadTitle = {onClickTitle}
-        onClickHeadValue = {onClickValue}/>
-      <tbody>{getArticles}</tbody>
+            { getArticleListHeader }
+            <tbody>{ getArticles }</tbody>
     </table>);
 
   };
